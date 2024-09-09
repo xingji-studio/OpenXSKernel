5d6503964ae4188889c5f39d15d4634d7ab679fc
Guoqiyu1115 <guoqiyu1115@163.com>
2024-09-09 21:00:37 +0800

    e

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,6 +3,6 @@ extern "C" {
 }
 #include "../include/include.h"
 
-extern "C" void KernelMain(const FrameBufferConfig &fbc, EFI_SYSTEM_TABLE &SystemTable, EFI_SIMPLE_POINTER_PROTOCOL &SPP, BOOT_CONFIG *BootConfig) {
+extern "C" void KernelMain(const FrameBufferConfig &fbc, EFI_SYSTEM_TABLE &SystemTable, BOOT_CONFIG *BootConfig) {
   // kernel
 }
